7b74a8d6e1a35c489c343214c58fd4caed97c058
banxi1988 <banxi1988@gmail.com>
2018-06-04 20:29:58 +0800

    feat 完善 storage 封装逻辑，增加 Settings 类及 asSetting 装饰器函数

--- src/storage.ts
+++ src/storage.ts
@@ -1,11 +1,50 @@
-export const enum Key {
-  auth = "auth"
-}
-
-export function getByKey(key: Key) {
+export function getByKey(key: string) {
   return wx.getStorageSync(key);
 }
 
-export function setByKey(key: Key, value: any) {
+export function setByKey(key: string, value: any) {
   return wx.setStorageSync(key, value);
 }
+
+/**
+ * @param defaultValue 设置项的默认值
+ * @param key 自定义其他的 key 值, 自定义 key 值可以避免代码 ugly 之后，属性名改变，然后
+ *  读取不到对应值。
+ */
+function asSetting(defaultValue: any, key?: string) {
+  return function(
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDecorator
+  ) {
+    const pkey = key || propertyKey;
+    Object.defineProperty(target, pkey, {
+      get: function() {
+        const ret = getByKey(propertyKey);
+        if (typeof ret === "undefined") {
+          return defaultValue;
+        } else {
+          return ret;
+        }
+      },
+
+      set: function(value) {
+        setByKey(propertyKey, value);
+      }
+    });
+  } as any;
+}
+
+/**
+ * 用户偏好设置存储类
+ * `autoLogin` 和 `username` 是两个示范属性，可以删除
+ */
+class Settings {
+  @asSetting(false, "isAutoLogin")
+  autoLogin: boolean;
+
+  @asSetting(null)
+  username: string;
+}
+
+export const settings = new Settings();
